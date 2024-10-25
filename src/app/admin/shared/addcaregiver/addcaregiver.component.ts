a1b79e4f55f745b8f198cb04d9bae8b1e60e99c8
vasuyadav1220 <mayankyadav1203@gmail.com>
2024-10-25 18:17:36 +0530

    mayank

--- src/app/admin/shared/addcaregiver/addcaregiver.component.ts
+++ src/app/admin/shared/addcaregiver/addcaregiver.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addcaregiver',
   templateUrl: './addcaregiver.component.html',
   styleUrls: ['./addcaregiver.component.css']
 })
-export class AddcaregiverComponent {
+export class AddcaregiverComponent  implements OnInit {
+  caregiverForm!:FormGroup;
 
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.caregiverForm = this.fb.group({
+      caregiverName: ['', Validators.required],
+      certification: [''],
+      certificationDoc: [''],
+      email: ['', [Validators.required, Validators.email]],
+      address: [''],
+      serviceType: [''],
+      electronicSignature: [''],
+      certificationExpiryDate: [],
+      caregiverNote: [''],
+      daysBeforeExpiration: [''],
+      password: ['', Validators.required]
+    });
+  }
 }
